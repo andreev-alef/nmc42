74f00bdfed4c1bf133eec0a4d1e9115076e7bbbd
andreev_af <andreev_af@alef>
2018-10-24 17:27:37 +0700

    Базовая фиксация

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./css/main.css" />
+        <title></title>
+    </head>
+    <body>
+        <div class="body">
+            <?php
+            print '<hr />Alef<hr/>' . getdate()[0];
+            ?>            
+        </div>
+    </body>
+</html>
