35833bc3bdf9399f285a65d703514303a6577876
andreev_af <andreev_af@alef>
2018-12-19 17:39:29 +0700

    Добавление новых шрифтов

---
+++ styles/protostar/fonts/ttf2woff.cmd
@@ -0,0 +1,6 @@
+ttf2woff.exe PTN57F.ttf pt_sans_narrow.woff
+ttf2woff.exe PTN77F.ttf pt_sans_narrow_bold.woff
+ttf2woff.exe PTS55F.ttf pt_sans.woff
+ttf2woff.exe PTS56F.ttf pt_sans_italic.woff
+ttf2woff.exe PTS75F.ttf pt_sans_bold.woff
+ttf2woff.exe PTS76F.ttf pt_sans_bold_italic.woff
