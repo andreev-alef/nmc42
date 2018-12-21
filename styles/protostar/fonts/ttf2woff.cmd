2f9c58f571db9821749d9b29c41aa1be6a6bdc38
andreev_af <andreev_af@alef>
2018-12-21 14:19:42 +0700

    Добавление шрифтов

--- styles/protostar/fonts/ttf2woff.cmd
+++ styles/protostar/fonts/ttf2woff.cmd
@@ -1,16 +1,6 @@
 rem ( .{1,}?)(\.ttf)
 rem $1$2$1.woff
-ttf2woff.exe OpenSans-Bold.ttf OpenSans-Bold.woff
-ttf2woff.exe OpenSans-BoldItalic.ttf OpenSans-BoldItalic.woff
-ttf2woff.exe OpenSans-ExtraBold.ttf OpenSans-ExtraBold.woff
-ttf2woff.exe OpenSans-ExtraBoldItalic.ttf OpenSans-ExtraBoldItalic.woff
-ttf2woff.exe OpenSans-Italic.ttf OpenSans-Italic.woff
-ttf2woff.exe OpenSans-Light.ttf OpenSans-Light.woff
-ttf2woff.exe OpenSans-LightItalic.ttf OpenSans-LightItalic.woff
-ttf2woff.exe OpenSans-Regular.ttf OpenSans-Regular.woff
-ttf2woff.exe OpenSans-SemiBold.ttf OpenSans-SemiBold.woff
-ttf2woff.exe OpenSans-SemiBoldItalic.ttf OpenSans-SemiBoldItalic.woff
-ttf2woff.exe YanoneKaffeesatz-Bold.ttf YanoneKaffeesatz-Bold.woff
-ttf2woff.exe YanoneKaffeesatz-ExtraLight.ttf YanoneKaffeesatz-ExtraLight.woff
-ttf2woff.exe YanoneKaffeesatz-Light.ttf YanoneKaffeesatz-Light.woff
-ttf2woff.exe YanoneKaffeesatz-Regular.ttf YanoneKaffeesatz-Regular.woff
+ttf2woff.exe NotoSerif-Bold.ttf NotoSerif-Bold.woff
+ttf2woff.exe NotoSerif-BoldItalic.ttf NotoSerif-BoldItalic.woff
+ttf2woff.exe NotoSerif-Italic.ttf NotoSerif-Italic.woff
+ttf2woff.exe NotoSerif-Regular.ttf NotoSerif-Regular.woff
