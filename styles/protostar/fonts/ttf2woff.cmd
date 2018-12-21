ab963746990e4271fffd10f6f0e3f5af6d6c6e75
andreev_af <andreev_af@alef>
2018-12-21 14:07:09 +0700

    Добавление шрифтов

--- styles/protostar/fonts/ttf2woff.cmd
+++ styles/protostar/fonts/ttf2woff.cmd
@@ -1,6 +1,16 @@
-ttf2woff.exe PTN57F.ttf pt_sans_narrow.woff
-ttf2woff.exe PTN77F.ttf pt_sans_narrow_bold.woff
-ttf2woff.exe PTS55F.ttf pt_sans.woff
-ttf2woff.exe PTS56F.ttf pt_sans_italic.woff
-ttf2woff.exe PTS75F.ttf pt_sans_bold.woff
-ttf2woff.exe PTS76F.ttf pt_sans_bold_italic.woff
+rem ( .{1,}?)(\.ttf)
+rem $1$2$1.woff
+ttf2woff.exe OpenSans-Bold.ttf OpenSans-Bold.woff
+ttf2woff.exe OpenSans-BoldItalic.ttf OpenSans-BoldItalic.woff
+ttf2woff.exe OpenSans-ExtraBold.ttf OpenSans-ExtraBold.woff
+ttf2woff.exe OpenSans-ExtraBoldItalic.ttf OpenSans-ExtraBoldItalic.woff
+ttf2woff.exe OpenSans-Italic.ttf OpenSans-Italic.woff
+ttf2woff.exe OpenSans-Light.ttf OpenSans-Light.woff
+ttf2woff.exe OpenSans-LightItalic.ttf OpenSans-LightItalic.woff
+ttf2woff.exe OpenSans-Regular.ttf OpenSans-Regular.woff
+ttf2woff.exe OpenSans-SemiBold.ttf OpenSans-SemiBold.woff
+ttf2woff.exe OpenSans-SemiBoldItalic.ttf OpenSans-SemiBoldItalic.woff
+ttf2woff.exe YanoneKaffeesatz-Bold.ttf YanoneKaffeesatz-Bold.woff
+ttf2woff.exe YanoneKaffeesatz-ExtraLight.ttf YanoneKaffeesatz-ExtraLight.woff
+ttf2woff.exe YanoneKaffeesatz-Light.ttf YanoneKaffeesatz-Light.woff
+ttf2woff.exe YanoneKaffeesatz-Regular.ttf YanoneKaffeesatz-Regular.woff
