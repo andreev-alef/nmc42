481f86aaa73278dc5db8e3c14569fef68cc74174
andreev_af <andreev_af@alef>
2018-12-29 11:13:19 +0700

    Добавлен шрифт Exo2

---
+++ styles/protostar/fonts/ttf/ttf2woff.cmd
@@ -0,0 +1,21 @@
+rem ( .{1,}?)(\.ttf)
+rem $1$2$1.woff
+
+ttf2woff.exe Exo2-Black.ttf Exo2-Black.woff
+ttf2woff.exe Exo2-BlackItalic.ttf Exo2-BlackItalic.woff
+ttf2woff.exe Exo2-Bold.ttf Exo2-Bold.woff
+ttf2woff.exe Exo2-BoldItalic.ttf Exo2-BoldItalic.woff
+ttf2woff.exe Exo2-ExtraBold.ttf Exo2-ExtraBold.woff
+ttf2woff.exe Exo2-ExtraBoldItalic.ttf Exo2-ExtraBoldItalic.woff
+ttf2woff.exe Exo2-ExtraLight.ttf Exo2-ExtraLight.woff
+ttf2woff.exe Exo2-ExtraLightItalic.ttf Exo2-ExtraLightItalic.woff
+ttf2woff.exe Exo2-Italic.ttf Exo2-Italic.woff
+ttf2woff.exe Exo2-Light.ttf Exo2-Light.woff
+ttf2woff.exe Exo2-LightItalic.ttf Exo2-LightItalic.woff
+ttf2woff.exe Exo2-Medium.ttf Exo2-Medium.woff
+ttf2woff.exe Exo2-MediumItalic.ttf Exo2-MediumItalic.woff
+ttf2woff.exe Exo2-Regular.ttf Exo2-Regular.woff
+ttf2woff.exe Exo2-SemiBold.ttf Exo2-SemiBold.woff
+ttf2woff.exe Exo2-SemiBoldItalic.ttf Exo2-SemiBoldItalic.woff
+ttf2woff.exe Exo2-Thin.ttf Exo2-Thin.woff
+ttf2woff.exe Exo2-ThinItalic.ttf Exo2-ThinItalic.woff
--- styles/protostar/fonts/ttf2woff.cmd
+++
@@ -1,6 +0,0 @@
-rem ( .{1,}?)(\.ttf)
-rem $1$2$1.woff
-ttf2woff.exe NotoSerif-Bold.ttf NotoSerif-Bold.woff
-ttf2woff.exe NotoSerif-BoldItalic.ttf NotoSerif-BoldItalic.woff
-ttf2woff.exe NotoSerif-Italic.ttf NotoSerif-Italic.woff
-ttf2woff.exe NotoSerif-Regular.ttf NotoSerif-Regular.woff
